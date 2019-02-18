89904ddb5bb9f98cab59b897fe9edf8d9803263a
tinnkm <husong@techfuser.net>
2019-02-18 16:15:09 +0800

    添加动态注册的前置代码，动态注册的逻辑不应该和job运行端在一起，所以写的时候没有依赖elastic-job，之后如需要迁移只需将zkutils和registerController移到合适位置

--- src/main/java/com/tinnkm/elasticjob/controller/registerController.java
+++ src/main/java/com/tinnkm/elasticjob/controller/registerController.java
@@ -1,59 +1,35 @@
 package com.tinnkm.elasticjob.controller;
 
-import com.dangdang.ddframe.job.api.simple.SimpleJob;
-import com.dangdang.ddframe.job.config.JobCoreConfiguration;
-import com.dangdang.ddframe.job.config.simple.SimpleJobConfiguration;
-import com.dangdang.ddframe.job.event.JobEventConfiguration;
-import com.dangdang.ddframe.job.lite.config.LiteJobConfiguration;
 import com.dangdang.ddframe.job.lite.lifecycle.domain.JobSettings;
-import com.dangdang.ddframe.job.lite.spring.api.SpringJobScheduler;
-import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
-import org.apache.curator.CuratorZookeeperClient;
+import com.tinnkm.elasticjob.utils.ZKUtils;
 import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.recipes.cache.ChildData;
-import org.apache.curator.framework.recipes.cache.NodeCache;
-import org.apache.curator.framework.recipes.cache.NodeCacheListener;
-import org.apache.curator.framework.recipes.cache.TreeCache;
-import org.apache.zookeeper.ZooKeeper;
-import org.apache.zookeeper.data.Stat;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.apache.zookeeper.ZKUtil;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import static com.tinnkm.elasticjob.config.SimpleJobConfig.PACKAGE_NAME;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/elastic-job")
 public class registerController {
-    @Autowired
-    private ZookeeperRegistryCenter regCenter;
 
-    @Autowired
-    private JobEventConfiguration jobEventConfiguration;
+    @Value("${elastic-job.serverList}")
+    private String serverList;
+    @Value("${elastic-job.namespace}")
+    private String namespace;
 
     @PostMapping("/register")
-    public String register(@RequestBody JobSettings jobSettings){
-
-        // 通过反射获取实现所有实现
-        SimpleJob job = null;
-        try {
-            Class<? extends SimpleJob> jobClass = (Class<? extends SimpleJob>) Class.forName(jobSettings.getJobClass());
-            job = jobClass.newInstance();
-//                Method setMongoTemplate = job.getClass().getMethod("setMongoTemplate", MongoTemplate.class);
-//                setMongoTemplate.invoke(job,mongoTemplate);
-        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException  e) {
-            e.printStackTrace();
-            return "当前工程下没有job实现类，请检查ip是否正确";
-        }
-        new SpringJobScheduler(job, regCenter,getLiteJobConfiguration(job.getClass(), jobSettings.getJobName(),jobSettings.getCron(), Integer.valueOf(jobSettings.getShardingTotalCount()), jobSettings.getShardingItemParameters(), PACKAGE_NAME +jobSettings.getJobShardingStrategyClass(),jobSettings.getJobParameter()),jobEventConfiguration ).init();
+    public String register(@RequestBody JobSettings jobSettings) throws IOException {
+        ZKUtils zkUtils = new ZKUtils(serverList, namespace);
+        zkUtils.addCacheData("/register-job-node");
+        zkUtils.persist("/register-job-node/"+jobSettings.getJobName(),new ObjectMapper().writeValueAsString(jobSettings));
+        zkUtils.close();
         return "success";
     }
 
-    private LiteJobConfiguration getLiteJobConfiguration(final Class<? extends SimpleJob> jobClass,final String jobName, final String cron, final int shardingTotalCount, final String shardingItemParameters,String strategyClass,String jobParameter) {
-        if (strategyClass.contains("PerformanceStrategy")){
-            return LiteJobConfiguration.newBuilder(new SimpleJobConfiguration(JobCoreConfiguration.newBuilder(
-                    jobName, cron, shardingTotalCount).jobParameter(jobParameter).shardingItemParameters(shardingItemParameters).failover(true).build(), jobClass.getCanonicalName())).monitorExecution(true).monitorPort(9888).reconcileIntervalMinutes(10).overwrite(true).jobShardingStrategyClass(strategyClass).build();
-        }
-        return LiteJobConfiguration.newBuilder(new SimpleJobConfiguration(JobCoreConfiguration.newBuilder(
-                jobName, cron, shardingTotalCount).jobParameter(jobParameter).shardingItemParameters(shardingItemParameters).failover(true).build(), jobClass.getCanonicalName())).monitorPort(9888).monitorExecution(true).reconcileIntervalMinutes(10).overwrite(true).build();
-    }
+
 }
---
+++ src/main/java/com/tinnkm/elasticjob/utils/ZKUtils.java
@@ -0,0 +1,318 @@
+package com.tinnkm.elasticjob.utils;
+import com.google.common.base.Charsets;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.ACLBackgroundPathAndBytesable;
+import org.apache.curator.framework.api.ACLProvider;
+import org.apache.curator.framework.api.transaction.CuratorTransactionBridge;
+import org.apache.curator.framework.recipes.cache.ChildData;
+import org.apache.curator.framework.recipes.cache.TreeCache;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.utils.CloseableUtils;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * zk工具类，用完记得关闭连接
+ */
+@Slf4j
+public class ZKUtils {
+    private  ThreadLocal<CuratorFramework> threadLocal = new ThreadLocal<>();
+    private final Map<String, TreeCache> caches = new HashMap<>();
+    private String serverLists;
+    private String namespace;
+    private int baseSleepTimeMilliseconds;
+    private int maxRetries;
+    private int maxSleepTimeMilliseconds;
+    private int sessionTimeoutMilliseconds;
+    private int connectionTimeoutMilliseconds;
+    private String digest;
+
+    public ZKUtils(String serverLists, String namespace, int baseSleepTimeMilliseconds, int maxRetries, int maxSleepTimeMilliseconds, int sessionTimeoutMilliseconds, int connectionTimeoutMilliseconds, String digest) {
+        this.serverLists = serverLists;
+        this.namespace = namespace;
+        this.baseSleepTimeMilliseconds = baseSleepTimeMilliseconds;
+        this.maxRetries = maxRetries;
+        this.maxSleepTimeMilliseconds = maxSleepTimeMilliseconds;
+        this.sessionTimeoutMilliseconds = sessionTimeoutMilliseconds;
+        this.connectionTimeoutMilliseconds = connectionTimeoutMilliseconds;
+        this.digest = digest;
+        init();
+    }
+
+    public ZKUtils(String serverLists, String namespace) {
+        this.serverLists = serverLists;
+        this.namespace = namespace;
+        this.baseSleepTimeMilliseconds = 1000;
+        this.maxRetries = 3;
+        this.maxSleepTimeMilliseconds = 3000;
+        init();
+    }
+
+    /**
+     * 初始化客户端
+     * @return 客户端
+     */
+    private void init(){
+        if (null != threadLocal.get()){
+            return ;
+        }
+        CuratorFrameworkFactory.Builder builder = CuratorFrameworkFactory.builder().connectString(serverLists).retryPolicy(new ExponentialBackoffRetry(baseSleepTimeMilliseconds, maxRetries, maxSleepTimeMilliseconds)).namespace(namespace);
+        if (0 != sessionTimeoutMilliseconds) {
+            builder.sessionTimeoutMs(sessionTimeoutMilliseconds);
+        }
+
+        if (0 != connectionTimeoutMilliseconds) {
+            builder.connectionTimeoutMs(connectionTimeoutMilliseconds);
+        }
+
+        if (!Strings.isNullOrEmpty(digest)) {
+            builder.authorization("digest", digest.getBytes(Charsets.UTF_8)).aclProvider(new ACLProvider() {
+                @Override
+                public List<ACL> getDefaultAcl() {
+                    return ZooDefs.Ids.CREATOR_ALL_ACL;
+                }
+
+                @Override
+                public List<ACL> getAclForPath(String path) {
+                    return ZooDefs.Ids.CREATOR_ALL_ACL;
+                }
+            });
+        }
+
+        CuratorFramework client = builder.build();
+
+        client.start();
+
+        try {
+            if (!client.blockUntilConnected(maxSleepTimeMilliseconds * maxRetries, TimeUnit.MILLISECONDS)) {
+                client.close();
+                throw new KeeperException.OperationTimeoutException();
+            }
+            threadLocal.set(client);
+        } catch (Exception e) {
+            log.error("初始化客户端失败",e);
+        }
+    }
+
+    /**
+     * 关闭客户端
+     */
+    public void close() {
+        CuratorFramework curatorFramework = threadLocal.get();
+        Iterator i$ = this.caches.entrySet().iterator();
+        while(i$.hasNext()) {
+            Map.Entry<String, TreeCache> each = (Map.Entry)i$.next();
+            ((TreeCache)each.getValue()).close();
+        }
+        if (null != curatorFramework){
+            this.waitForCacheClose();
+            CloseableUtils.closeQuietly(curatorFramework);
+            threadLocal.remove();
+        }
+    }
+
+    private void waitForCacheClose() {
+        try {
+            Thread.sleep(500L);
+        } catch (InterruptedException var2) {
+            Thread.currentThread().interrupt();
+        }
+
+    }
+
+    /**
+     * 获取值
+     * @param key key
+     * @return 值
+     */
+    public String get(String key) {
+        TreeCache cache = this.findTreeCache(key);
+        if (null == cache) {
+            return this.getDirectly(key);
+        } else {
+            ChildData resultInCache = cache.getCurrentData(key);
+            if (null != resultInCache) {
+                return null == resultInCache.getData() ? null : new String(resultInCache.getData(), Charsets.UTF_8);
+            } else {
+                return this.getDirectly(key);
+            }
+        }
+    }
+
+    private TreeCache findTreeCache(String key) {
+        Iterator i$ = this.caches.entrySet().iterator();
+
+        Map.Entry entry;
+        do {
+            if (!i$.hasNext()) {
+                return null;
+            }
+
+            entry = (Map.Entry)i$.next();
+        } while(!key.startsWith((String)entry.getKey()));
+
+        return (TreeCache)entry.getValue();
+    }
+
+    public String getDirectly(String key) {
+        try {
+            CuratorFramework curatorFramework = threadLocal.get();
+            return new String(curatorFramework.getData().forPath(key), Charsets.UTF_8);
+        } catch (Exception var3) {
+            return null;
+        }
+    }
+
+    public List<String> getChildrenKeys(String key) {
+        try {
+            CuratorFramework curatorFramework = threadLocal.get();
+            List result = curatorFramework.getChildren().forPath(key);
+            Collections.sort(result, Comparator.reverseOrder());
+            return result;
+        } catch (Exception var3) {
+            return Collections.emptyList();
+        }
+    }
+
+    public int getNumChildren(String key) {
+        try {
+            CuratorFramework curatorFramework = threadLocal.get();
+            Stat stat = curatorFramework.checkExists().forPath(key);
+            if (null != stat) {
+                return stat.getNumChildren();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return 0;
+    }
+
+    public boolean isExisted(String key) {
+        try {
+            CuratorFramework curatorFramework = threadLocal.get();
+            return null != curatorFramework.checkExists().forPath(key);
+        } catch (Exception var3) {
+            return false;
+        }
+    }
+
+    public void persist(String key, String value) {
+        try {
+            if (!this.isExisted(key)) {
+                CuratorFramework curatorFramework = threadLocal.get();
+                ((ACLBackgroundPathAndBytesable)curatorFramework.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT)).forPath(key, value.getBytes(Charsets.UTF_8));
+            } else {
+                this.update(key, value);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void update(String key, String value) {
+        try {
+            CuratorFramework curatorFramework = threadLocal.get();
+            curatorFramework.inTransaction().check().forPath(key).and().setData().forPath(key, value.getBytes(Charsets.UTF_8)).and().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void persistEphemeral(String key, String value) {
+        try {
+            CuratorFramework curatorFramework = threadLocal.get();
+            if (this.isExisted(key)) {
+                curatorFramework.delete().deletingChildrenIfNeeded().forPath(key);
+            }
+
+            ((ACLBackgroundPathAndBytesable)curatorFramework.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL)).forPath(key, value.getBytes(Charsets.UTF_8));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String persistSequential(String key, String value) {
+        try {
+            CuratorFramework curatorFramework = threadLocal.get();
+            return (String)((ACLBackgroundPathAndBytesable)curatorFramework.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT_SEQUENTIAL)).forPath(key, value.getBytes(Charsets.UTF_8));
+        } catch (Exception var4) {
+            return null;
+        }
+    }
+
+    public void persistEphemeralSequential(String key) {
+        try {
+            CuratorFramework curatorFramework = threadLocal.get();
+            ((ACLBackgroundPathAndBytesable)curatorFramework.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL_SEQUENTIAL)).forPath(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void remove(String key) {
+        try {
+            CuratorFramework curatorFramework = threadLocal.get();
+            curatorFramework.delete().deletingChildrenIfNeeded().forPath(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public long getRegistryCenterTime(String key) {
+        long result = 0L;
+
+        try {
+            this.persist(key, "");
+            CuratorFramework curatorFramework = threadLocal.get();
+            result = ((Stat)curatorFramework.checkExists().forPath(key)).getMtime();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Preconditions.checkState(0L != result, "Cannot get registry center time.");
+        return result;
+    }
+
+
+    public void addCacheData(String cachePath) {
+        CuratorFramework curatorFramework = threadLocal.get();
+        TreeCache cache = new TreeCache(curatorFramework, cachePath);
+
+        try {
+            cache.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        this.caches.put(cachePath + "/", cache);
+    }
+
+    public void evictCacheData(String cachePath) {
+        TreeCache cache = this.caches.remove(cachePath + "/");
+        if (null != cache) {
+            cache.close();
+        }
+
+    }
+
+    public Object getRawCache(String cachePath) {
+        return this.caches.get(cachePath + "/");
+    }
+}
