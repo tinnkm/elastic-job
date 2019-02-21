c9426e2d9266ccdc3dc76e69f0885633a9cb5d43
tinnkm <husong@techfuser.net>
2019-02-21 18:40:40 +0800

    修改动态创建逻辑，新增更新，删除任务

--- src/main/java/com/tinnkm/elasticjob/config/JobApplicaionRunner.java
+++ src/main/java/com/tinnkm/elasticjob/config/JobApplicaionRunner.java
@@ -4,11 +4,16 @@ import com.dangdang.ddframe.job.api.simple.SimpleJob;
 import com.dangdang.ddframe.job.event.JobEventConfiguration;
 import com.dangdang.ddframe.job.lite.config.LiteJobConfiguration;
 import com.dangdang.ddframe.job.lite.internal.config.LiteJobConfigurationGsonFactory;
+import com.dangdang.ddframe.job.lite.internal.storage.JobNodePath;
+import com.dangdang.ddframe.job.lite.lifecycle.api.JobAPIFactory;
 import com.dangdang.ddframe.job.lite.lifecycle.domain.JobSettings;
 import com.dangdang.ddframe.job.lite.spring.api.SpringJobScheduler;
 import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
 import com.dangdang.ddframe.job.util.env.IpUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.recipes.cache.ChildData;
@@ -20,6 +25,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -61,8 +67,43 @@ public class JobApplicaionRunner implements ApplicationRunner  {
                 saveSystemInfoToZk();
             }
         } ,0,1000*60*1);
+        // 默认加载所有
+        jobInit();
         // 监听节点变化
         watchNode();
+    }
+
+    private void jobInit() {
+        List<String> childrenKeys = regCenter.getChildrenKeys("/register-job-node");
+        childrenKeys.forEach(childrenKey -> {
+            String jobSettingStr = regCenter.get("/register-job-node/"+childrenKey);
+            if (!StringUtils.isEmpty(jobSettingStr)){
+                addJob(jobSettingStr);
+            }
+
+        });
+    }
+
+    private void addJob(String jobSettingStr) {
+        LiteJobConfiguration liteJobConfiguration = LiteJobConfigurationGsonFactory.fromJson(jobSettingStr);
+        ObjectMapper objectMapper = new ObjectMapper();
+        JobSettings jobSettings = null;
+        try {
+            jobSettings = objectMapper.readValue(jobSettingStr, JobSettings.class);
+        } catch (IOException e) {
+            log.error("节点存储数据有误！",e);
+        }
+        // 通过反射获取实现所有实现
+        SimpleJob job = null;
+        try {
+            Class<? extends SimpleJob> jobClass = (Class<? extends SimpleJob>) Class.forName(jobSettings.getJobClass());
+            job = jobClass.newInstance();
+            Method setMongoTemplate = job.getClass().getMethod("setMongoTemplate", MongoTemplate.class);
+            setMongoTemplate.invoke(job,mongoTemplate);
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | InvocationTargetException | NoSuchMethodException e) {
+            log.error("当前工程下没有job实现类，请检查ip是否正确",e);
+        }
+        new SpringJobScheduler(job, regCenter,liteJobConfiguration,jobEventConfiguration ).init();
     }
 
     private void watchNode() throws Exception {
@@ -72,30 +113,41 @@ public class JobApplicaionRunner implements ApplicationRunner  {
             ChildData data = treeCacheEvent.getData();
             TreeCacheEvent.Type eventType = treeCacheEvent.getType();
             if (data != null){
-                if (eventType.equals(TreeCacheEvent.Type.NODE_ADDED) || eventType.equals(TreeCacheEvent.Type.NODE_UPDATED)){
-                    List<String> childrenKeys = regCenter.getChildrenKeys("/register-job-node");
-                    childrenKeys.forEach(childrenKey -> {
-                        String jobSettingStr = regCenter.get("/register-job-node/"+childrenKey);
-                        LiteJobConfiguration liteJobConfiguration = LiteJobConfigurationGsonFactory.fromJson(jobSettingStr);
-                        ObjectMapper objectMapper = new ObjectMapper();
-                        JobSettings jobSettings = null;
-                        try {
-                            jobSettings = objectMapper.readValue(jobSettingStr, JobSettings.class);
-                        } catch (IOException e) {
-                            log.error("节点存储数据有误！",e);
+                String jobSettingStr = new String(data.getData());
+                JobSettings jobSettings = null;
+                ObjectMapper objectMapper = new ObjectMapper();
+                try {
+                    jobSettings = objectMapper.readValue(jobSettingStr, JobSettings.class);
+                } catch (IOException e) {
+                    log.error("数据转换异常！",e);
+                }
+                if (null == jobSettings){
+                    throw new IllegalArgumentException("节点存储数据有误！");
+                }
+                switch (eventType){
+                    case NODE_ADDED:
+                        // 添加job
+                        addJob(jobSettingStr);
+                        break;
+                    case NODE_UPDATED:
+                        // 更新job
+                        Preconditions.checkArgument(!Strings.isNullOrEmpty(jobSettings.getJobName()), "jobName can not be empty.");
+                        Preconditions.checkArgument(!Strings.isNullOrEmpty(jobSettings.getCron()), "cron can not be empty.");
+                        Preconditions.checkArgument(jobSettings.getShardingTotalCount() > 0, "shardingTotalCount should larger than zero.");
+                        JobNodePath jobNodePath = new JobNodePath(jobSettings.getJobName());
+                        regCenter.update(jobNodePath.getConfigNodePath(), LiteJobConfigurationGsonFactory.toJsonForObject(jobSettings));
+                        break;
+                    case NODE_REMOVED:
+                        // 删除job
+                        if (!StringUtils.isEmpty(jobSettings.getJobName())){
+                            JobNodePath jobNodePaths = new JobNodePath(jobSettings.getJobName());
+                            for (String each : regCenter.getChildrenKeys(jobNodePaths.getInstancesNodePath())) {
+                                regCenter.remove(jobNodePaths.getInstanceNodePath(each));
+                            }
                         }
-                        // 通过反射获取实现所有实现
-                        SimpleJob job = null;
-                        try {
-                            Class<? extends SimpleJob> jobClass = (Class<? extends SimpleJob>) Class.forName(jobSettings.getJobClass());
-                            job = jobClass.newInstance();
-                            Method setMongoTemplate = job.getClass().getMethod("setMongoTemplate", MongoTemplate.class);
-                            setMongoTemplate.invoke(job,mongoTemplate);
-                        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | InvocationTargetException | NoSuchMethodException e) {
-                            log.error("当前工程下没有job实现类，请检查ip是否正确",e);
-                        }
-                        new SpringJobScheduler(job, regCenter,liteJobConfiguration,jobEventConfiguration ).init();
-                    });
+                        break;
+                    default:
+                        break;
                 }
             }
         });
